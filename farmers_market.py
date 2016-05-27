8c50c328dac0406a23fc48a0944cb5fb81b37e66
Aaron Held <aarondheld@gmail.com>
2016-05-27 07:38:07 -0400

    simpler start point

--- farmers_market.py
+++ farmers_market.py
@@ -4,10 +4,8 @@ import requests
 CSV_URL = 'http://data.phl.opendata.arcgis.com/datasets/0707c1f31e2446e881d680b0a5ee54bc_0.csv'
 
 def get_farmers_data():
-    download = requests.get(CSV_URL, stream=True)
-    cr = csv.reader(download.iter_lines(decode_unicode='utf-8'), delimiter=',')
-    my_list = list(cr)
-    return my_list
+    farmers_file = requests.get(CSV_URL, stream=True)
+    return []
         
 if __name__ == '__main__':
      print(get_farmers_data())
