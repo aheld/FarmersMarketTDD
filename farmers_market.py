d550f9c9e59e06c6aaefe88e4c4828fd8dd5ca99
Aaron Held <aarondheld@gmail.com>
2016-05-26 21:52:27 -0400

    step-2

--- farmers_market.py
+++ farmers_market.py
@@ -1,13 +1,27 @@
 import csv
 import requests
+from geopy.distance import vincenty
 
 CSV_URL = 'http://data.phl.opendata.arcgis.com/datasets/0707c1f31e2446e881d680b0a5ee54bc_0.csv'
 
 def get_farmers_data():
     download = requests.get(CSV_URL, stream=True)
-    cr = csv.DictReader(download.iter_lines(decode_unicode='utf-8'), delimiter=',')
+    cr = csv.DictReader((x.decode('utf-8-sig') 
+                         for x in download.iter_lines()
+                         ), delimiter=',')
     my_list = list(cr)
     return my_list
         
+def find_nearest(farmers_markets):
+    market_street = (39.95654, -75.19631)
+    records = []
+    for rec in farmers_markets:
+        lat_log = (rec["Y"], rec["X"])
+        rec['dist'] = vincenty(market_street, lat_log).miles
+        if rec['dist'] < 1:
+            records.append(rec)
+    return records
+
+
 if __name__ == '__main__':
-     print(get_farmers_data())
+     print( find_nearest(get_farmers_data()))
--- test_farmers_market.py
+++ test_farmers_market.py
@@ -2,12 +2,11 @@ import requests
 import responses
 from unittest.mock import MagicMock, patch
 
-from farmers_market import get_farmers_data, CSV_URL
+from farmers_market import get_farmers_data, find_nearest, CSV_URL
 
 
 @responses.activate
-def test_my_api():
-     
+def test_my_api(): 
     sample_data = open('farmers_market_test.csv', 'rb').read()
     responses.add(responses.GET, CSV_URL,
                   body=sample_data, status=200,
@@ -22,3 +21,20 @@ def test_my_api():
   
     assert len(responses.calls) == 1
     assert responses.calls[0].request.url == CSV_URL
+    
+def test_find_nearest():
+    farmers_markets = [{
+        "Y": 39.95653,
+        "X": -75.19630,
+        "name": "near"
+    },{
+        "Y": 30.95653,
+        "X": -70.19630,
+        "name": "far"
+    }
+    ]
+    
+    nearest = find_nearest(farmers_markets)
+    assert len(nearest) == 1
+    
+    
