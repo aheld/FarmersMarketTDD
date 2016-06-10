a4491d0d743df24441957a8d7e89560cefbec2d5
Aaron Held <aarondheld@gmail.com>
2016-06-10 10:03:29 -0400

    encoding issues

--- farmers_market.py
+++ farmers_market.py
@@ -1,4 +1,6 @@
+import csv
 from geopy.distance import vincenty
+import requests
 
 
 def find_closest(markets):
@@ -12,6 +14,17 @@ def add_distance_info(market):
         (-75.1991014, 39.9567835)
     ).miles
     return market
+
+
+def get_farmers_market_data(url):
+    response = requests.get(url, stream=True)
+    markets = csv.DictReader(response.iter_lines(decode_unicode=True), delimiter=',')
+    return list(markets)
+
+
+def load_markets():
+    pass
+
 
 if __name__ == '__main__':
     print(find_closest())
--- test_farmers_market.py
+++ test_farmers_market.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 from unittest.mock import patch
+import responses
 
-from farmers_market import add_distance_info, find_closest
+from farmers_market import (add_distance_info, find_closest,
+                            get_farmers_market_data)
 
 
 class TestMarkets(TestCase):
@@ -37,9 +39,16 @@ class TestMarkets(TestCase):
 
 class TestMarketsLoader(TestCase):
 
-    @patch('farmers_market.add_distance_info')
-    def test_load_market(self, mock_add_distance_info):
-        find_closest({"bad": "data"})
-        assert mock_add_distance_info.called
-        assert mock_add_distance_info.assert_called_once_with([{'a': 1},
-                                                               {'b': 2}])
+    @responses.activate
+    def test_can_load_market(self):
+        CSV_URL = 'http://data.phl.opendata.arcgis.com/datasets/0707c1f31e2446e881d680b0a5ee54bc_0.csv'
+        test_csv = open('farmers_market_test.csv').read()
+        responses.add(responses.GET, CSV_URL,
+                  body=test_csv, status=200,
+                  stream=True
+                  )
+        market_data = get_farmers_market_data(CSV_URL)
+        assert market_data == TEST_CSV_DATA
+
+
+TEST_CSV_DATA = [{'ADDRESS': '18th and Christian St', 'MONTHS': 'June - November', 'DAY': 'Thurs', 'ADDRESS_NOTES': 'at the YMCA', 'ACCEPT_PHILLY_FOOD_BUCKS': 'Y', 'Y': '39.940841916695057', 'TIME': '3-7pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '1', 'ACCEPT_SNAP_ACCESS': 'Y', 'NEIGHBORHOOD': 'South', '\ufeffX': '-75.172701252171919', 'MAJOR_BUS_SUBWAY_ROUTES': '2, 17', 'NAME': '18th and Christian*', 'ZIP': '19146'}, {'ADDRESS': '22nd and Tasker St', 'MONTHS': 'End June/Early July – November', 'DAY': 'Tues', 'ADDRESS_NOTES': '', 'ACCEPT_PHILLY_FOOD_BUCKS': 'Y', 'Y': '39.932178999784682', 'TIME': '2-6pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '2', 'ACCEPT_SNAP_ACCESS': 'Y', 'NEIGHBORHOOD': 'South', '\ufeffX': '-75.181423846357561', 'MAJOR_BUS_SUBWAY_ROUTES': '7, 29', 'NAME': '22nd & Tasker*', 'ZIP': '19145'}, {'ADDRESS': '26th St and Allegheny Ave', 'MONTHS': 'July 8th - November', 'DAY': 'Wed', 'ADDRESS_NOTES': '', 'ACCEPT_PHILLY_FOOD_BUCKS': 'Y', 'Y': '40.004104443998102', 'TIME': '1-5pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '3', 'ACCEPT_SNAP_ACCESS': 'Y', 'NEIGHBORHOOD': 'North', '\ufeffX': '-75.172283714309913', 'MAJOR_BUS_SUBWAY_ROUTES': '48, 60', 'NAME': '26th and Allegheny*', 'ZIP': '19129'}, {'ADDRESS': '29th and Wharton St', 'MONTHS': 'June – November', 'DAY': 'Tues', 'ADDRESS_NOTES': '', 'ACCEPT_PHILLY_FOOD_BUCKS': 'Y', 'Y': '39.937297083281905', 'TIME': '2-6pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '4', 'ACCEPT_SNAP_ACCESS': 'Y', 'NEIGHBORHOOD': 'South', '\ufeffX': '-75.1920388833027', 'MAJOR_BUS_SUBWAY_ROUTES': '12, 64', 'NAME': '29th & Wharton*', 'ZIP': '19146'}, {'ADDRESS': '33rd & Diamond Sts', 'MONTHS': 'June -November', 'DAY': 'Thurs', 'ADDRESS_NOTES': 'In front of Mander Playground', 'ACCEPT_PHILLY_FOOD_BUCKS': 'Y', 'Y': '39.988682579276201', 'TIME': '2-6pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '5', 'ACCEPT_SNAP_ACCESS': 'Y', 'NEIGHBORHOOD': 'North', '\ufeffX': '-75.187063935483323', 'MAJOR_BUS_SUBWAY_ROUTES': '7, 32, 39, 54, 61', 'NAME': '33rd & Diamond*', 'ZIP': '19121'}, {'ADDRESS': '4th St & Lehigh Ave', 'MONTHS': 'July 7th - November', 'DAY': 'Tues', 'ADDRESS_NOTES': '', 'ACCEPT_PHILLY_FOOD_BUCKS': 'Y', 'Y': '39.991869006387248', 'TIME': '1-5pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '6', 'ACCEPT_SNAP_ACCESS': 'Y', 'NEIGHBORHOOD': 'North', '\ufeffX': '-75.138355706471373', 'MAJOR_BUS_SUBWAY_ROUTES': '47, 54', 'NAME': '4th and Lehigh*', 'ZIP': '19133'}, {'ADDRESS': '52nd St and Haverford Ave', 'MONTHS': 'Early July – November', 'DAY': 'Wed', 'ADDRESS_NOTES': '', 'ACCEPT_PHILLY_FOOD_BUCKS': 'Y', 'Y': '39.964913169362674', 'TIME': '1-5pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '7', 'ACCEPT_SNAP_ACCESS': 'Y', 'NEIGHBORHOOD': 'West ', '\ufeffX': '-75.224594826060098', 'MAJOR_BUS_SUBWAY_ROUTES': '30, 52', 'NAME': '52nd & Haverford*', 'ZIP': '19139'}, {'ADDRESS': '58th and Chester Street', 'MONTHS': 'June -November', 'DAY': 'Wed', 'ADDRESS_NOTES': '', 'ACCEPT_PHILLY_FOOD_BUCKS': 'Y', 'Y': '39.935749877733862', 'TIME': '2-6pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '8', 'ACCEPT_SNAP_ACCESS': 'Y', 'NEIGHBORHOOD': 'Southwest', '\ufeffX': '-75.228537854068307', 'MAJOR_BUS_SUBWAY_ROUTES': '13 trolley, G', 'NAME': '58th & Chester*', 'ZIP': '19143'}, {'ADDRESS': '8th & Poplar Sts, 19123', 'MONTHS': 'May  – November', 'DAY': 'Wed', 'ADDRESS_NOTES': '', 'ACCEPT_PHILLY_FOOD_BUCKS': '', 'Y': '39.967988514708807', 'TIME': '3-6pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '9', 'ACCEPT_SNAP_ACCESS': 'Y', 'NEIGHBORHOOD': 'Lower North', '\ufeffX': '-75.150519176937692', 'MAJOR_BUS_SUBWAY_ROUTES': '47', 'NAME': '8th & Poplar', 'ZIP': '19123'}, {'ADDRESS': '54th & Lindbergh Blvd', 'MONTHS': 'June-November', 'DAY': 'Thurs', 'ADDRESS_NOTES': 'In front of Bartrams Village Apartments', 'ACCEPT_PHILLY_FOOD_BUCKS': '', 'Y': '39.932759682718938', 'TIME': '3:30-7pm', 'ACCEPT_FMNP': 'Y', 'OBJECTID': '10', 'ACCEPT_SNAP_ACCESS': 'Y ', 'NEIGHBORHOOD': 'Southwest', '\ufeffX': '-75.216078112569335', 'MAJOR_BUS_SUBWAY_ROUTES': '36 trolley', 'NAME': 'Bartram’s Farm Market', 'ZIP': '19143'}]
