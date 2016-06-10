447bfbf1eb041878556b95b6d719b5548e8fe384
Aaron Held <aarondheld@gmail.com>
2016-06-10 09:06:46 -0400

    flake passing, new test

--- farmers_market.py
+++ farmers_market.py
@@ -1,15 +1,17 @@
 from geopy.distance import vincenty
 
+
 def find_closest(markets):
-    markets = map(add_distance_info, markets)        
+    markets = map(add_distance_info, markets)
     return min(markets, key=lambda x: x['dist'])
+
 
 def add_distance_info(market):
     market['dist'] = vincenty(
         (market['X'], market['Y']),
-        (-75.1991014,39.9567835)
+        (-75.1991014, 39.9567835)
     ).miles
     return market
 
 if __name__ == '__main__':
-     print(get_farmers_data())
+    print(find_closest())
--- test_farmers_market.py
+++ test_farmers_market.py
@@ -1,36 +1,43 @@
 from unittest import TestCase
+from unittest.mock import patch
 
 from farmers_market import add_distance_info, find_closest
 
 
 class TestMarkets(TestCase):
     def setUp(self):
-        self.sample_market_data =[
-            {'X': -75.17270125,
-            'Y': 39.94084192,
-            'NAME': '18th and Christian*'},
-            {'X':-75.20926373,
-            'Y':39.94955888,
-            'NAME':'Clark Park*'}
-            ]
-    
+        self.sample_market_data = [
+                {'X': -75.17270125,
+                 'Y': 39.94084192,
+                 'NAME': '18th and Christian*'},
+                {'X': -75.20926373,
+                 'Y': 39.94955888,
+                 'NAME': 'Clark Park*'}
+                ]
+
     def test_decorate_market(self):
-        sample_market = {'X':-75.20926373,
-                            'Y':39.94955888}
+        sample_market = {'X': -75.20926373,
+                         'Y': 39.94955888}
         assert 'dist' not in sample_market.keys()
-        
-        decorated = add_distance_info( sample_market )
-                
+
+        decorated = add_distance_info(sample_market)
         assert 'dist' in decorated.keys()
-    
+
     def test_find_dist(self):
         clark_park = add_distance_info(self.sample_market_data[1])
         assert clark_park['dist'] < 1
-        
+
         christian = add_distance_info(self.sample_market_data[0])
         assert christian['dist'] > 1
 
-
-    def test_find_the_closest(self):    
+    def test_find_the_closest(self):
         closest_park = find_closest(self.sample_market_data)
         assert closest_park['NAME'] == 'Clark Park*'
+
+
+class TestMarketsLoader(TestCase):
+
+    @patch('farmers_market.add_distance_info')
+    def test_load_market(self, mock_add_distance_info):
+        find_closest({"bad": "data"})
+        assert mock_add_distance_info.called
